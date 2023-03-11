862be10c5422f7d58dce05b10678a8b79ab30608
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2023-03-11 11:00:42 +0530

    Update Login.js

--- pages/Login.js
+++ pages/Login.js
@@ -1,47 +1,34 @@
 import React from 'react'
 import Image from "next/image"
 import Link from 'next/link'
+import { ImFacebook2 } from 'react-icons/im';
+import { ImGoogle2 } from 'react-icons/im';
+import { ImLinkedin2 } from 'react-icons/im';
+
+
 const Signup = () => {
     return (
         <div>            
-            <section className="vh-100">
+            <section className=" mx-10 vh-100">
             <div className="container-fluid h-custom">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col-md-9 col-lg-6 col-xl-5">
-                        {/* <Image src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                            className="img-fluid" alt="Sample image" /> */}
+                    {<Image src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                    className="img-fluid" alt="Sample image" width="350px"
+                    height="400px"layout='responsive' /> }
                     </div>
-                    <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                    <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1" style={{top: "100px"}}>
                         <form>
-                            <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                                <p className="lead fw-normal mb-0 me-3">Sign in with</p>
-                                <button type="button" className="btn btn-primary btn-floating mx-1">
-                                    <i className="fab fa-facebook-f"></i>
-                                </button>
-
-                                <button type="button" className="btn btn-primary btn-floating mx-1">
-                                    <i className="fab fa-twitter"></i>
-                                </button>
-
-                                <button type="button" className="btn btn-primary btn-floating mx-1">
-                                    <i className="fab fa-linkedin-in"></i>
-                                </button>
-                            </div>
-
-                            <div className="divider d-flex align-items-center my-4">
-                                <p className="text-center fw-bold mx-3 mb-0">Or</p>
-                            </div>
-
                             <div className="form-outline mb-4">
+                            <label className="form-label" htmlFor="E-mail">Email address</label>
                                 <input type="email" id="e-mail" className="form-control form-control-lg"
                                     placeholder="Enter a valid email address" />
-                                <label className="form-label" htmlFor="E-mail">Email address</label>
                             </div>
 
                             <div className="form-outline mb-3">
+                            <label className="form-label" htmlFor="Passwrd">Password</label>
                                 <input type="password" id="password" className="form-control form-control-lg"
                                     placeholder="Enter password" />
-                                <label className="form-label" htmlFor="Passwrd">Password</label>
                             </div>
 
                             <div className="d-flex justify-content-between align-items-center">
@@ -60,29 +47,25 @@ const Signup = () => {
                                 <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <Link href="/Signup"
                                     className="link-danger">Register</Link></p>
                             </div>
+                            <div>
+                                <p style={{paddingTop:"30px",paddingLeft: "240px"}}> <strong>Or</strong></p>
+                            </div>
+                            <div>
+                                <p style={{paddingLeft: "210px"}}>Sign in with</p>
+                                <button type="button" className="btn btn-primary btn-floating btn-block">
+                                    <i className='mx-1'><ImFacebook2/></i>Continue with Facebook
+                                </button>
 
+                                <button type="button" className="btn btn-primary btn-floating  btn-block">
+                                    <i className='mx-1'><ImGoogle2/></i>Continue with Google
+                                </button>
+
+                                <button type="button" className="btn btn-primary btn-floating  btn-block">
+                                <i className='mx-2'><ImLinkedin2/></i>Continue with LinkedIn
+                                </button>
+                            </div>
                         </form>
                     </div>
-                </div>
-            </div>
-            <div
-                className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-                <div className="text-white mb-3 mb-md-0">
-                    Copyright © 2020. All rights reserved.
-                </div>
-                <div>
-                    <a href="#!" className="text-white me-4">
-                        <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="#!" className="text-white me-4">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                    <a href="#!" className="text-white me-4">
-                        <i className="fab fa-google"></i>
-                    </a>
-                    <a href="#!" className="text-white">
-                        <i className="fab fa-linkedin-in"></i>
-                    </a>
                 </div>
             </div>
         </section>
