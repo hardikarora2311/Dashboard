639b7a18b293cb4464cab1afd22e862225cd4320
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2023-03-11 10:28:15 +0000

    Signup updated

--- pages/Signup.js
+++ pages/Signup.js
@@ -4,29 +4,14 @@ import { ImGoogle2 } from 'react-icons/im';
 import { ImLinkedin2 } from 'react-icons/im';
 import Image from 'next/dist/client/image';
 const Signup = () => {
-    const [Name, setname] = useState();
+    const [Name, setName] = useState();
     const [Email, setEmail] = useState();
-    const [pass, setpass] = useState();
+    const [pass, setPass] = useState();
 
-    const handleChange = (e) => {
-        if (e.target.name === 'Name') {
-            setname(e.target.value);
-            console.log(e.target.value)
-        }
-        else if (e.target.name === 'email') {
-            setEmail(e.target.value)
-            console.log(e.target.value)
-        }
-        else if (e.target.name === 'pass') {
-            setpass(e.target.value)
-            console.log(e.target.value)
-        }
-    }
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(e.Name.value);
         const data = { Name, Email, pass }
-        let res = await fetch("http://localhost:3000/Signup", {
+        let res = await fetch("http://localhost:3000/api/signup", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -34,10 +19,6 @@ const Signup = () => {
             body: JSON.stringify(data),
         })
         let response = await res.json()
-        console.log(response)
-        setEmail('');
-        setname('');
-        setpass('');
     }
     return (
         <>
@@ -53,22 +34,22 @@ const Signup = () => {
                             <form onSubmit={handleSubmit} method='POST'>
                                 <div className="form-outline mb-4">
                                 <label className="form-label" htmlFor="name">Name</label>
-                                    <input value={Name} onChange={handleChange} type="text" id="name" className="form-control form-control-lg"
+                                    <input value={Name} onChange={(e)=>{setName(e.target.value)}} type="text" id="name" className="form-control form-control-lg"
                                         placeholder="Enter your Name" />
                                 </div>
                                 <div className="form-outline mb-4">
                                 <label className="form-label" htmlFor="email">Email address</label>
-                                    <input value={Email} onChange={handleChange} type="email" id="email" className="form-control form-control-lg"
+                                    <input value={Email} onChange={(e)=>{setEmail(e.target.value)}} type="email" id="email" className="form-control form-control-lg"
                                         placeholder="Enter a valid email address" />
                                 </div>
 
                                 <div className="form-outline mb-3">
                                 <label className="form-label" htmlFor="pass">Password</label>
-                                    <input value={pass} onChange={handleChange} type="password" id="pass" className="form-control form-control-lg"
+                                    <input value={pass} onChange={(e)=>{setPass(e.target.value)}} type="password" id="pass" className="form-control form-control-lg"
                                         placeholder="Enter password" />   
                                 </div>
                                 <div className="text-center text-lg-start mt-4 pt-2">
-                                    <button type="button" className="btn btn-primary btn-lg"
+                                    <button type="submit" className="btn btn-primary btn-lg"
                                         style={{ paddingleft: "2.5rem", paddingright: "2.5rem" }}>Signup</button>
                                 </div>
                                 <div>
