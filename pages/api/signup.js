3ecb8dcec94afc3a3ddae3bea58c3c3d4b6d9e8f
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2023-03-11 21:26:26 +0530

    Update signup.js

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,12 +1,13 @@
 import data from "../../models/User";
 import cntdb from "../../middleware/mongoose";
 
+var Crypto=require("crypto-js");
 const handler=async(req,res)=>{
     if(req.method==='POST'){
         console.log(req.body);
-        let u=new data(req.body);
+        const {Name,Email}=req.body;
+        let u=new data({Name,Email,pass:Crypto.AES.encrypt(req.body.pass,"secret123").toString()});
         await u.save();
-
         res.status(200).json({success:"sucess"});
     } 
     else{
