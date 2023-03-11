40d04e5084c943cf0f5db7ba217bb6985871d183
Ambuj Bhandari <90816837+Ambuj-Bhandari@users.noreply.github.com>
2023-03-11 21:26:07 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,20 @@
-import User from "../../models/User";
+import data from "../../models/User";
 import cntdb from "../../middleware/mongoose";
-
+var Crypto=require("crypto-js");
 const handler=async(req,res)=>{
     if(req.method==='POST'){
-        // console.log(req.body);
-        // let user= await User.findOne({email:req.body.email})
-        // if(user){
-        // if(req.body.email===user.email && req.body.password===user.password)
-            res.status(200).json({success:"sucess",email,Name});
-        //}
+        console.log(req.body);
+        let user= await data.findOne({Email:req.body.Email})
+        const bytes=Crypto.AES.decrypt(user.pass,"secret123");
+        var decryptpass=bytes.toString(Crypto.enc.Utf8);
+        if(user){
+        if(req.body.Email===user.Email &&req.body.pass=== decryptpass)
+            res.status(200).json({success:true,Email:user.Email,Name:user.Name});
+        else
+            res.status(200).json({success:false,error:"Invalid Credentials"});
+        }
+        else
+            res.status(200).json({success:false,error:"No User found"});
     } 
     else{
         res.status(400).json({error:"This method is not allowed"});
